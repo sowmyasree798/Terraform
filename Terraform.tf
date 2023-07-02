c83179b6278e8263c4c5084346cede5c236331c0
sowmyasree798 <45842285+sowmyasree798@users.noreply.github.com>
2023-07-02 11:19:41 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "us-east-1"
   access_key = "AKIA57WCDEI77A3VC6HR"
   secret_key = "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"
 }
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "AWSServer" {
   ami             = "ami-06b09bfacae1453cb"
   instance_type   = "t2.micro"
-  key_name        = "ashokitnewkey"
+  key_name        = "linuxkeypair"
   security_groups = ["default"]
   tags = {
     Name = "MyEC2-VM"
